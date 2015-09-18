74dc5c8d835f2a7493ed7104f43874b26595ef8b
magic-lantern <magic-lantern@users.noreply.github.com>
2015-09-17 19:39:25 -0600

    Finished up problem list infobuttons

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,7 @@ problems:
   Hypertensive disorder - SNOMEDCT 38341003 
   Transformed migraine - SNOMEDCT 427419006
   Hypercholesterolemia - SNOMEDCT 13644009
+  Bacterial pneumonia - SNOMEDCT 53084003
 sample of problem URLs
   http://service.oib.utah.edu:8080/infobutton-service/infoRequest?representedOrganization.id.root=http://axeium.net&xsltTransform=Infobutton_UI_AXEIUM&taskContext.c.c=PROBLISTREV&mainSearchCriteria.v.c=38341003&mainSearchCriteria.v.cs=2.16.840.1.113883.6.96&mainSearchCriteria.v.dn=Hypertensive disorder&encounter.c.c=AMB&informationRecipient=PROV&informationRecipient.languageCode.c=en&informationRecipient.healthCareProvider.c.c=200000000X&performer=PROV&performer.languageCode.c=en&performer.healthCareProvider.c.c=200000000X&knowledgeResponseType=application/json
   http://service.oib.utah.edu:8080/infobutton-service/infoRequest?representedOrganization.id.root=http://axeium.net&xsltTransform=Infobutton_UI_AXEIUM&taskContext.c.c=PROBLISTREV&mainSearchCriteria.v.c=427419006&mainSearchCriteria.v.cs=2.16.840.1.113883.6.96&mainSearchCriteria.v.dn=Transformed migraine&encounter.c.c=AMB&informationRecipient=PROV&informationRecipient.languageCode.c=en&informationRecipient.healthCareProvider.c.c=200000000X&performer=PROV&performer.languageCode.c=en&performer.healthCareProvider.c.c=200000000X&knowledgeResponseType=application/json
@@ -31,12 +32,12 @@ sample of lab test URLS:
   http://service.oib.utah.edu:8080/infobutton-service/infoRequest?representedOrganization.id.root=http://axeium.net&xsltTransform=Infobutton_UI_AXEIUM&taskContext.c.c=LABRREV&mainSearchCriteria.v.c=2571-8&mainSearchCriteria.v.cs=2.16.840.1.113883.6.1&mainSearchCriteria.v.dn=Triglycerides, Serum or Plasma&encounter.c.c=AMB&informationRecipient=PROV&informationRecipient.languageCode.c=en&informationRecipient.healthCareProvider.c.c=200000000X&performer=PROV&performer.languageCode.c=en&performer.healthCareProvider.c.c=200000000X&knowledgeResponseType=application/json
   http://service.oib.utah.edu:8080/infobutton-service/infoRequest?representedOrganization.id.root=http://axeium.net&xsltTransform=Infobutton_UI_AXEIUM&taskContext.c.c=LABRREV&mainSearchCriteria.v.c=664-3&mainSearchCriteria.v.cs=2.16.840.1.113883.6.1&mainSearchCriteria.v.dn=Stat Gram Stain&encounter.c.c=AMB&informationRecipient=PROV&informationRecipient.languageCode.c=en&informationRecipient.healthCareProvider.c.c=200000000X&performer=PROV&performer.languageCode.c=en&performer.healthCareProvider.c.c=200000000X&knowledgeResponseType=application/json
 */
-$.ajaxSetup({timeout:4000});
+$.ajaxSetup({timeout:8000});
 
 var infoButtonModel = Backbone.Model.extend({
   defaults: {
-    'mainSearchCriteria.v.c' : '',
-    'mainSearchCriteria.v.dn' : ''
+    'mainsearchcriteria_v_c' : '',   //toLower() and replace . with _ for easier use
+    'mainsearchcriteria_v_dn' : ''   //toLower() and replace . with _ for easier use
   },
   validate: function(attributes){
     // add this later
@@ -46,8 +47,8 @@ var infoButtonModel = Backbone.Model.extend({
   },
   urlRoot: REST_SERVICE_ROOT_URL,
   url : function() {
-    return  this.urlRoot + "&mainSearchCriteria.v.c=" + this.attributes['mainSearchCriteria.v.c'] + 
-      "&mainSearchCriteria.v.dn=" + this.attributes['mainSearchCriteria.v.dn'];
+    return  this.urlRoot + "&mainSearchCriteria.v.c=" + this.attributes.mainsearchcriteria_v_c + 
+      "&mainSearchCriteria.v.dn=" + this.attributes.mainsearchcriteria_v_dn;
   },
   parse: function(response) {
     // map server response to expected object attributes
@@ -57,25 +58,56 @@ var infoButtonModel = Backbone.Model.extend({
   }
 });
 
-var test = new infoButtonModel();
-test.set("mainSearchCriteria.v.c", 38341003);
-test.set("mainSearchCriteria.v.dn", 'Hypertensive disorder');
-test.fetch({
-  success: function(model, response, options){
-    console.log("Retrieved infobutton information: ", response);
+var problemView = Backbone.View.extend({
+  el: '#infoModal',
+  template: _.template( $('#problemViewTemplate').html() ),
+  initalize: function() {
+    console.log("64 - called initalize");
+    this.render();
   },
-  error: function(model, response, options){
-    console.log("Could not retrieve infobutton information: " , response);
+  render : function() {
+    console.log("68 - called render");
+    console.log("this.model.attributes: ", this.model.attributes);
+    this.$el.html( this.template(this.model.attributes) );
+    return this;
+  }
+});
+
+// from http://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript
+function getHostname(href) {
+  var location = document.createElement("a");
+  location.href = href;
+  if (location.host == "")
+    location.href = location.href;
+  return location.hostname;
+  }
+
+var models = {};
+$(".problembutton").each(function(index, element) {
+  var de = $(element).data("mainsearchcriteria_v_c");
+  if (de) {
+    models[de] = new infoButtonModel();
+    models[de].set("mainsearchcriteria_v_c", de);
+    models[de].set("mainsearchcriteria_v_dn", $(element).data("mainsearchcriteria_v_dn"));
+    models[de].fetch({
+      success: function(model, response, options){
+        console.log("Retrieved infobutton information: ", response);
+        $(element).toggleClass('hidden');
+        $('.alert-info').addClass('hidden')
+      },
+      error: function(model, response, options){
+        console.log("Could not retrieve infobutton information: " , response);
+        $('.alert-warning').removeClass('hidden')
+      }
+    });
   }
 });
 
 $(function(){
-  $(".infobutton").click(function(){
-    $("#infoModal").modal('show');
-    var summary = test.get("feed")[0].entry[0].summary.value;
-    var link = test.get("feed")[0].entry[0].link[0].href;
-    $(".modal-title").html("test");
-    $(".modal-body").html(summary);
-    $(".modal-body").append("<p>For more information go to <a href=" + link + " target='_blank'>" + link + "<\a>");
+  $(".problembutton").click(function(e){
+    var m = $(e.currentTarget).data("mainsearchcriteria_v_c");
+    var tmpView = new problemView({model: models[m]});
+    tmpView.render();
+    $('#problemViewModal').modal('show');
   });
 })
